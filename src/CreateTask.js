6aea102e1f75da8a72b21e5fb5c6d098168a56e1
Luis Pirela <34918215+contrastereo@users.noreply.github.com>
2021-10-07 05:49:27 -0300

    Input sensibility added Now the text input turns red when the task length is too short or too long

--- src/CreateTask.js
+++ src/CreateTask.js
@@ -9,6 +9,14 @@ function CreateTask(props) {
   const handleChange = (e) => {
     setNewTask(e.target.value);
   };
+  //Imput warnings
+  const textWarnings = () => {
+    if (newTask.length < 5 && newTask.length > 0) {
+      return "Your task is too short";
+    } else if (newTask.length > 15) {
+      return "Your task is too large";
+    }
+  };
   //SUBMIT
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,17 +41,27 @@ function CreateTask(props) {
   }, []);
 
   return (
-    <div className="taskInput">
-      <input
-        placeholder="Do Something"
-        value={newTask}
-        onChange={(e) => {
-          handleChange(e);
-        }}
-      />
-      <button onClick={(e) => handleSubmit(e)} type="submit">
-        Go!
-      </button>
+    <div>
+      <div
+        className={
+          (newTask.length < 5 && newTask.length > 0) || newTask.length > 18
+            ? "invalidInput taskInput"
+            : "taskInput"
+        }
+      >
+        <input
+          placeholder="Do Something"
+          value={newTask}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <button onClick={(e) => handleSubmit(e)} type="submit">
+          Go!
+        </button>
+      </div>
+
+      <p className="warnings">{textWarnings()}</p>
     </div>
   );
 }
